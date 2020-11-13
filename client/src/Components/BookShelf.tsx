483dab873a81301c0792d49f5961322681470071
Matt Hurst <matt.adam.hurst@gmail.com>
2020-11-13 14:57:15 +0000

    add functionality allowing user to select genre/categories

--- client/src/Components/BookShelf.tsx
+++ client/src/Components/BookShelf.tsx
@@ -8,13 +8,11 @@ import './BookShelf.scss'
 
 interface myProps {
   books: Array<Book> | null;
-  category: string;
 }
 
 const BookShelf = (props: myProps) => {
   return (
     <>
-    <h2 className="bookShelfH2">{props.category}</h2>
     <div className="bookShelf">
       {props.books && props.books.map(book => <BookDisplay key={book._id} book={book} />
       )}
--- client/src/Pages/Bookcase.tsx
+++ client/src/Pages/Bookcase.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import BookShelf from '../Components/BookShelf';
 import { User } from '../Interfaces'
 import './Bookcase.scss'
@@ -8,12 +8,46 @@ interface myProps {
   name?: string;
 }
 
+//TODO: add state to store bookshelf genre/category
+
+// TODO: make h2 = state
+
 const Bookcase = (props: myProps) => {
+  const [selectedBooks, setSelectedBooks] = useState('all')
+
+  function handleChange (e: React.ChangeEvent<HTMLSelectElement>) {
+    setSelectedBooks(e.target.value)
+  }
+
   return (
     <div className="bookCase">
       <h1>{props.name ? `${props.name}'s`  : 'Your'} Library:</h1>
-      <BookShelf category={'All Books'} books={props.user.books} />
-      <BookShelf category={'Fiction'} books={props.user.books ? props.user.books.filter(book => book.genre?.toLocaleLowerCase() === 'fiction'): null} />
+      {/* <h2 className="bookCaseH2">{selectedBooks}</h2>  */}
+      <select onChange={handleChange} id="filterBooks">
+        <option selected value="all">All Books</option>
+        <option value="borrow">Available to Borrow</option>
+        <option value="star">Must Reads</option>
+        <option value="fiction">Fiction</option>
+        <option value="crime">Crime</option>
+        <option value="science fiction">Science Fiction</option>
+        <option value="fantasy">Fantasy</option>
+        <option value="politics and history">Politics and History</option>
+      </select> 
+
+      {props.user.books && <BookShelf 
+      books={selectedBooks === 'all' ? 
+      props.user.books : 
+      props.user.books?.filter(book => {
+        if (selectedBooks === 'borrow') return book.availableToBorrow; 
+        if (selectedBooks === 'star') return book.star; 
+        return book.genre?.toLowerCase() === selectedBooks;
+        })} />}
+      
+      <h2 className="bookCaseH2">Library Stats:</h2>
+      <p>{`Number of books in library: ${props.user.books ? props.user.books.length : null}`}</p>
+      <p>{`Number of books available to borrow: ${props.user.books ? props.user.books.filter(book => book.availableToBorrow).length : null}`}</p>
+      <p>{`Number of must reads: ${props.user.books ? props.user.books.filter(book => book.star).length : null}`}</p>
+    
     </div>
 
   )
