eee229638f9727bcbaef9bde785ce9a5d4904103
Matt Hurst <matt.adam.hurst@gmail.com>
2020-11-26 11:36:20 +0000

    add sign up page elements, styling, and initial logic

--- client/src/Pages/SignUp.tsx
+++ client/src/Pages/SignUp.tsx
@@ -1,8 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
+// import { useHistory } from "react-router-dom";
+// import { signup } from '../ApiService/serverApiService';
+import './SignUp.scss'
 
 const SignUp = () => {
+  const [formValues, setFormValues] = useState({name: '', password: '', confirmPassword: ''})
+  const [errorMessage, setErrorMessage] = useState('')
+
+  function onChange (e: React.ChangeEvent<HTMLInputElement>) {
+    setFormValues({
+      ...formValues,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  async function onSubmit (e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    if (formValues.password !== formValues.confirmPassword) setErrorMessage("Passwords do not match, try again")
+    setFormValues({name: '', password: '', confirmPassword: ''})
+    console.log(formValues)
+    // await login(formValues.name, formValues.password);
+    // history.go(0)
+  }
+
+
   return (
-    <h1>SIGN UP PAGE</h1>
+    <div className="SignUpPage">
+      <h1 className="SignUph1">Sign Up</h1>
+      { errorMessage && <p className="errorMessage">{errorMessage}</p> }
+      <div className="SignUpFormContainer">
+        <form onSubmit={onSubmit} className="SignUpForm">
+          <input type="text" placeholder="Username:" name="name" onChange={onChange} value={formValues.name}/>
+          <input type="password" placeholder="Password:" name="password" onChange={onChange} value={formValues.password}/>
+          <input type="password" placeholder="Confirm Password:" name="confirmPassword" onChange={onChange} value={formValues.confirmPassword}/>
+          <button type="submit" className="signupBTN">Sign Up</button>
+        </form>
+      </div>
+    </div>
+      
   )
 }
 
